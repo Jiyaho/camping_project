69d038892edc5cc77663d74b268a8379f09dbc2a
Jiyaho <masqu2n@gmail.com>
2024-02-03 03:10:07 +0900

    Fix: Props 관련 오류 해결

--- frontend/src/app/places/[id]/order/page.tsx
+++ frontend/src/app/places/[id]/order/page.tsx
@@ -1,18 +1,20 @@
 'use client';
 
 import { Header } from '@/components/Header';
+import GoToPrevPageButton from '@/components/common/Button/GoToPrevPageButton';
 import PageTitle from '@/components/common/Title/PageTitle';
 import OrderInformation from '@/components/elements/Places/Order/OrderInformation';
 import { prevArrowIcon } from '@/public/svgs';
-import { useParams } from 'next/navigation';
+import { useParams, useRouter } from 'next/navigation';
 
 function BookCampsite() {
   const params = useParams();
+  const router = useRouter();
 
   return (
     <>
       <Header />
-      <PageTitle href={`/places/${params.id}`} iconSrc={prevArrowIcon} title="예약하기" />
+      <GoToPrevPageButton text="예약하기" onClick={() => router.push(`/places/${params.id}`)} />
       <OrderInformation />
     </>
   );
