b0700002e41d646f6104b508607fe8de9332109e
Jiyaho <masqu2n@gmail.com>
2024-02-03 03:10:23 +0900

    Test: 네이버 로그인 테스트 진행

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -20,11 +20,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <GlobalStyle />
       <head>
-        <script
+        {/* <script
           type="text/javascript"
           src="https://static.nid.naver.com/js/naveridlogin_js_sdk_2.0.0.js"
           charSet="utf-8"
-        ></script>
+        ></script> */}
       </head>
       <body>
         <RecoilRootProvider>
--- frontend/src/components/Login/NaverLogin/NaverLogin.tsx
+++ frontend/src/components/Login/NaverLogin/NaverLogin.tsx
@@ -19,7 +19,7 @@ export default function NaverLogin() {
   };
 
   useEffect(() => {
-    initNaverLogin();
+    // initNaverLogin();
   }, []);
 
   return (
